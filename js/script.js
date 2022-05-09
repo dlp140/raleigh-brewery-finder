c55555d7a7e1d73052208a7a9f849ac305b26681
Dave <dlp140@gmail.com>
2022-05-09 11:42:51 -0400

    jQuery variables referencing HTML elements added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-alert("JavaScript is working");
+// alert("JavaScript is working");
+
+// Variables
+const URL =
+  "https://api.openbrewerydb.org/breweries?by_city=raleigh&per_page=100&page=1";
+console.log(URL);
+
+// Element references
+
+// Define jQuery variables as jQuery references to HTML elements
+const $name = $("#name");
+const $street = $("#street");
+const $city = $("#city");
+const $state = $("#state");
+const $phone = $("#phone");
+const $website = $("#website_url");
+const $findBreweryBtn = $("#breweryFinderBtn");
+const $mainContent = $("main");
+
+// Event Listeners
+// $randomBrewery.on("click", findBrewery);
+
+// Functions
+// const findBrewery = () => {
+//   const randomBrewery = Math.floor(Math.random() * URL.length);
+// };
